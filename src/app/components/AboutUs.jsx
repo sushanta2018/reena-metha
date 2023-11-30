1a73a226fd488c8debbdb9e0aa568d3f3db93940
sushant bhowmik <sushantabhowmik@metadesignsolutions.co.uk>
2023-11-30 12:19:26 +0530

    resolve eslint

--- src/app/components/AboutUs.jsx
+++ src/app/components/AboutUs.jsx
@@ -10,8 +10,8 @@ export default function AboutUs() {
             <Image src={ProfileImg} alt="" />
           </div>
           <div className="about-content">
-            <h2 className='font-workSans text-[38px] mb-[16px] md:text-[48px] font-extrabold leading-[48px] md:leading-[58px] text-black'>Who Is <span className="text-primary_01">Reena Mehta</span></h2>
-            <p className='font-openSans text-[16px] opacity-[0.8] leading-[24px] text-black'>An Indian vocalist embarking on a profound musical odyssey. Raised in the heartland of India, I discovered my musical identity within the rich tapestry of classical Indian ragas. Yet, my artistic vision knows no bounds, as I delve into a multifarious array of musical genres, harmoniously weaving together melodies that narrate the intricate stories of life's myriad vicissitudes.</p>
+            <h2 className="font-workSans text-[38px] mb-[16px] md:text-[48px] font-extrabold leading-[48px] md:leading-[58px] text-black">Who Is <span className="text-primary_01">Reena Mehta</span></h2>
+            <p className="font-openSans text-[16px] opacity-[0.8] leading-[24px] text-black">An Indian vocalist embarking on a profound musical odyssey. Raised in the heartland of India, I discovered my musical identity within the rich tapestry of classical Indian ragas. Yet, my artistic vision knows no bounds, as I delve into a multifarious array of musical genres, harmoniously weaving together melodies that narrate the intricate stories of lifes myriad vicissitudes.</p>
             <button type="button" className="text-white bg-primary_01 rounded-[52px] p-[16px] font-openSans text-[18px] mt-[48px] min-w-[198px] hover:opacity-[0.8]">Read More</button>
           </div>
         </div>
--- src/app/components/CollaboratesGenres.jsx
+++ src/app/components/CollaboratesGenres.jsx
@@ -6,10 +6,10 @@ export default function CollaboratesGenres() {
     <section className="collaborates-wrapper bg-no-repeat bg-cover py-[80px]">
       <div className="container px-4 mx-auto 2xl:max-w-[1140px]">
         <div className="md:flex">
-          <h2 className='md:w-6/12 font-workSans text-[38px] md:mb-[52px] mb-[20px] md:text-[48px] font-extrabold leading-[48px] md:leading-[58px] text-black'><span className="text-primary_01">Collaborates</span> Across Genres</h2>
-          <p className='md:w-6/12 font-openSans text-[16px] opacity-[0.8] leading-[24px] text-black md:mb-[52px] mb-[30px]'> Yet, my artistic vision knows no bounds, as I delve into a multifarious array of musical genres, harmoniously weaving together melodies that narrate the intricate stories of life's myriad vicissitudes.</p>
+          <h2 className="md:w-6/12 font-workSans text-[38px] md:mb-[52px] mb-[20px] md:text-[48px] font-extrabold leading-[48px] md:leading-[58px] text-black"><span className="text-primary_01">Collaborates</span> Across Genres</h2>
+          <p className="md:w-6/12 font-openSans text-[16px] opacity-[0.8] leading-[24px] text-black md:mb-[52px] mb-[30px]"> Yet, my artistic vision knows no bounds, as I delve into a multifarious array of musical genres, harmoniously weaving together melodies that narrate the intricate stories of lifes myriad vicissitudes.</p>
         </div>
-        <div className='rounded-2xl overflow-hidden'><Image className="w-full" src={CollaborateImg} alt="" /></div>
+        <div className="rounded-2xl overflow-hidden"><Image className="w-full" src={CollaborateImg} alt="" /></div>
       </div>
     </section>
   )
